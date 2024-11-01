a5bba534df8e4bf513100a090d7030724772895e
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-10-31 19:34:05 -0500

    Initial commit

---
+++ src/main/java/com/cibertec/crud_tienda/CrudTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.crud_tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudTiendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/crud_tienda/CrudTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.crud_tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
